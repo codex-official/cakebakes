3bf9c32f009561a87c7c8d98b9c1bb1c0cc39833
codex-official <77504210+codex-official@users.noreply.github.com>
2024-04-08 19:35:03 +0530

    Update adminpanel.js
    Fixed Shopping Cart

--- js/adminpanel.js
+++ js/adminpanel.js
@@ -28,78 +28,208 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 
     const products = ref(database, "products");
 
-
+    let productList=[]
+    var productAttr;
+    const parent= document.getElementById('parent');
 
     function display() {
+      
         onValue(products, function (snapshot) {
           let chatsArray = Object.values(snapshot.val());
           for (let i=0;i<chatsArray.length;i++) {
             var list = Object.values(chatsArray[i])
             var link = 'https://firebasestorage.googleapis.com/v0/b/cakebakes-ef849.appspot.com/o/1711620494204-product-1.jpg?alt=media&token=d25f44ab-f4ba-42c3-9cfa-c9c1ea121768'
-            console.log(list[0])
+            
+
+            productAttr  = {
+              name: list[2],
+              price: list[3],
+            }
+            productList.push(productAttr);
+
+            
+            
+            
+const div1 = document.createElement("div");
+div1.appendChild(document.createTextNode(''));
+div1.className = "col-lg-3 col-md-6 col-sm-6"
+
+const div2 = document.createElement("div");
+div2.appendChild(document.createTextNode(''));
+div2.className = "product__item"
+div1.appendChild(div2);
+
+const div3 = document.createElement("div");
+div3.appendChild(document.createTextNode(''));
+div3.className = "product__item__pic set-bg"
+div3.style.backgroundImage = 'url('+list[0]+')';
+div2.appendChild(div3);
+
+const div4 = document.createElement("div");
+div4.appendChild(document.createTextNode(""));
+div4.className = "product__label"
+div3.appendChild(div4);
+
+const span= document.createElement("span");
+span.appendChild(document.createTextNode("Cupcake"));
+div4.appendChild(span);
+
+const div5 = document.createElement("div");
+div5.appendChild(document.createTextNode(""));
+div5.className = "product__item__text"
+div2.appendChild(div5);
+
+const h6 = document.createElement("h6");
+const a1 = document.createElement("a");
+a1.appendChild(document.createTextNode(list[2]))
+h6.appendChild(a1);
+div5.appendChild(h6);
+h6.style.color = "#111111";
+h6.style.fontWeight = "600";
+h6.style.textTransform = "uppercase";
+
+const div6 = document.createElement("div");
+div6.appendChild(document.createTextNode(list[3]));
+div6.className = "product__item__price"
+div5.appendChild(div6);
+
+const div7 = document.createElement("div");
+div7.appendChild(document.createTextNode(""));
+div7.className = "cart_add"
+div5.appendChild(div7);
+
+const a = document.createElement("a");
+a.appendChild(document.createTextNode("Add to Cart"))
+a.id="product" + i;
+a.style.cursor = "pointer"
+a.addEventListener('click', (x)=>{
+ 
+      let currentId = document.getElementById(x.srcElement.id);
+      let itemName =
+        currentId.parentElement.previousElementSibling.previousElementSibling
+          .firstElementChild.innerHTML;
+          console.log(itemName)
+      let itemPrice = currentId.parentElement.previousElementSibling.innerHTML;
+      let imgdiv =
+        currentId.parentElement.parentElement.previousElementSibling.style
+          .backgroundImage;
+
+          let productCur = { name: itemName, price: itemPrice, img: imgdiv, qty:1 };
+          let productList = JSON.parse(localStorage.getItem("productList")) || [];
+          productList.push(productCur);
+          localStorage.setItem("productList", JSON.stringify(productList));
+          
+          
+      // localStorage.setItem("cartID" + itemCount, [itemName, itemPrice, imgdiv]);
+      // // console.log(itemName)
+      // localStorage.setItem("itemCount", itemCount);
+      // var cartCount = document.getElementById("cartCount").innerText;
+      // cartCount += 1;
+      // document.getElementById("cartCount").innerText = cartCount;
+});
+div7.appendChild(a);
+
+
+
+// console.log(div1);
+parent.appendChild(div1);
+
+            
+          };
+
+          
+          // return (parent.innerHTML += productList.map((x) => {`
+          //   <div class="col-lg-3 col-md-6 col-sm-6">
+          //   <div class="product__item">
+          //     <div
+          //       class="product__item__pic set-bg"
+          //       data-setbg="img/shop/product-2.jpg"
+          //       style="background-image: url(img/shop/product-2.jpg);"
+          //     >
+          //       <div class="product__label">
+          //         <span>Cupcake</span>
+          //       </div>
+          //     </div>
+          //     <div class="product__item__text">
+          //       <h6><a href="#">${list[2]}</a></h6>
+          //       <div class="product__item__price">${list[3]}</div>
+          //       <div class="cart_add">
+          //         <a href="#" id="two" onclick="addtoCart(this)">Add to cart</a>
+          //       </div>
+          //     </div>
+          //   </div>
+          // </div>
+          //   `
+          //   })); 
+            
             
               
-                const parent= document.getElementById('parent');
+                
 
-                const div1 = document.createElement("div");
-                div1.appendChild(document.createTextNode(''));
-                div1.className = "col-lg-3 col-md-6 col-sm-6"
+                // const div1 = document.createElement("div");
+                // div1.appendChild(document.createTextNode(''));
+                // div1.className = "col-lg-3 col-md-6 col-sm-6"
 
-                const div2 = document.createElement("div");
-                div2.appendChild(document.createTextNode(''));
-                div2.className = "product__item"
-                div1.appendChild(div2);
+                // const div2 = document.createElement("div");
+                // div2.appendChild(document.createTextNode(''));
+                // div2.className = "product__item"
+                // div1.appendChild(div2);
 
-                const div3 = document.createElement("div");
-                div3.appendChild(document.createTextNode(''));
-                div3.className = "product__item__pic set-bg"
-                var image = list[0];
-                div3.style.backgroundImage = 'url('+image+')';
-                div2.appendChild(div3);
+                // const div3 = document.createElement("div");
+                // div3.appendChild(document.createTextNode(''));
+                // div3.className = "product__item__pic set-bg"
+                // var image = list[0];
+                // div3.style.backgroundImage = 'url('+image+')';
+                // div2.appendChild(div3);
 
-                const div4 = document.createElement("div");
-                div4.appendChild(document.createTextNode(""));
-                div4.className = "product__label"
-                div3.appendChild(div4);
+                // const div4 = document.createElement("div");
+                // div4.appendChild(document.createTextNode(""));
+                // div4.className = "product__label"
+                // div3.appendChild(div4);
 
-                const span= document.createElement("span");
-                span.appendChild(document.createTextNode(list[1]));
-                div4.appendChild(span);
+                // const span= document.createElement("span");
+                // span.appendChild(document.createTextNode(list[1]));
+                // div4.appendChild(span);
 
-                const div5 = document.createElement("div");
-                div5.appendChild(document.createTextNode(""));
-                div5.className = "product__item__text"
-                div2.appendChild(div5);
+                // const div5 = document.createElement("div");
+                // div5.appendChild(document.createTextNode(""));
+                // div5.className = "product__item__text"
+                // div2.appendChild(div5);
 
-                const h6 = document.createElement("h6");
-                h6.appendChild(document.createTextNode(list[2]));
-                div5.appendChild(h6);
-                h6.style.color = "#111111";
-                h6.style.fontWeight = "600";
-                h6.style.textTransform = "uppercase";
+                // const h6 = document.createElement("h6");
+                // const a1 = document.createElement('a');
+                // a1.appendChild(document.createTextNode(list[2]));
+                // h6.appendChild(a1);
+                // div5.appendChild(h6);
+                // h6.style.color = "#111111";
+                // h6.style.fontWeight = "600";
+                // h6.style.textTransform = "uppercase";
 
-                const div6 = document.createElement("div");
-                div6.appendChild(document.createTextNode(list[3]));
-                div6.className = "product__item__price"
-                div5.appendChild(div6);
+                // const div6 = document.createElement("div");
+                // div6.appendChild(document.createTextNode(list[3]));
+                // div6.className = "product__item__price"
+                // div5.appendChild(div6);
 
-                const div7 = document.createElement("div");
-                div7.appendChild(document.createTextNode(""));
-                div7.className = "cart_add"
-                div5.appendChild(div7);
+                // const div7 = document.createElement("div");
+                // div7.appendChild(document.createTextNode(""));
+                // div7.className = "cart_add"
+                // div5.appendChild(div7);
 
-                const a = document.createElement("a");
-                a.appendChild(document.createTextNode("Add to Cart"));
-                div7.appendChild(a);
+                // const a = document.createElement("a");
+                // a.appendChild(document.createTextNode("Add to Cart"));
+                // a.style.cursor="pointer";
+                // a.addEventListener('click',() => {
+                //   addtoCart(this)
+                // })
+                // div7.appendChild(a);
+            
 
 
-                parent.appendChild(div1);
-
+                // parent.appendChild(div1);
             
             
-            
-            
-          }
+          
+          
           
         });
       }
@@ -107,58 +237,55 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 
 // const parent= document.getElementById('parent');
 
-// const div1 = document.createElement("div");
-// div1.appendChild(document.createTextNode(''));
-// div1.className = "col-lg-3 col-md-6 col-sm-6"
+const div1 = document.createElement("div");
+div1.appendChild(document.createTextNode(''));
+div1.className = "col-lg-3 col-md-6 col-sm-6"
 
-// const div2 = document.createElement("div");
-// div2.appendChild(document.createTextNode(''));
-// div2.className = "product__item"
-// div1.appendChild(div2);
+const div2 = document.createElement("div");
+div2.appendChild(document.createTextNode(''));
+div2.className = "product__item"
+div1.appendChild(div2);
 
-// const div3 = document.createElement("div");
-// div3.appendChild(document.createTextNode(''));
-// div3.className = "product__item__pic set-bg"
-// div3.style.backgroundImage = "url(img/shop/product-1.jpg)";
-// div2.appendChild(div3);
+const div3 = document.createElement("div");
+div3.appendChild(document.createTextNode(''));
+div3.className = "product__item__pic set-bg"
+div3.style.backgroundImage = "url(img/shop/product-1.jpg)";
+div2.appendChild(div3);
 
-// const div4 = document.createElement("div");
-// div4.appendChild(document.createTextNode(""));
-// div4.className = "product__label"
-// div3.appendChild(div4);
+const div4 = document.createElement("div");
+div4.appendChild(document.createTextNode(""));
+div4.className = "product__label"
+div3.appendChild(div4);
 
-// const span= document.createElement("span");
-// span.appendChild(document.createTextNode("Cupcake"));
-// div4.appendChild(span);
+const span= document.createElement("span");
+span.appendChild(document.createTextNode("Cupcake"));
+div4.appendChild(span);
 
-// const div5 = document.createElement("div");
-// div5.appendChild(document.createTextNode(""));
-// div5.className = "product__item__text"
-// div2.appendChild(div5);
+const div5 = document.createElement("div");
+div5.appendChild(document.createTextNode(""));
+div5.className = "product__item__text"
+div2.appendChild(div5);
 
-// const h6 = document.createElement("h6");
-// h6.appendChild(document.createTextNode("test"));
-// div5.appendChild(h6);
-// h6.style.color = "#111111";
-// h6.style.fontWeight = "600";
-// h6.style.textTransform = "uppercase";
+const h6 = document.createElement("h6");
+h6.appendChild(document.createTextNode("test"));
+div5.appendChild(h6);
+h6.style.color = "#111111";
+h6.style.fontWeight = "600";
+h6.style.textTransform = "uppercase";
 
-// const div6 = document.createElement("div");
-// div6.appendChild(document.createTextNode("$32.00"));
-// div6.className = "product__item__price"
-// div5.appendChild(div6);
+const div6 = document.createElement("div");
+div6.appendChild(document.createTextNode("$32.00"));
+div6.className = "product__item__price"
+div5.appendChild(div6);
 
-// const div7 = document.createElement("div");
-// div7.appendChild(document.createTextNode(""));
-// div7.className = "cart_add"
-// div5.appendChild(div7);
+const div7 = document.createElement("div");
+div7.appendChild(document.createTextNode(""));
+div7.className = "cart_add"
+div5.appendChild(div7);
 
-// const a = document.createElement("a");
-// a.appendChild(document.createTextNode("Add to Cart"));
-// div7.appendChild(a);
-
-
-// console.log(div1);
-// parent.appendChild(div1);
+const a = document.createElement("a");
+a.appendChild(document.createTextNode("Add to Cart"));
+div7.appendChild(a);
+parent.appendChild(div1);
 
 

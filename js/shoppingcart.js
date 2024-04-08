239f0db5f39f0f3fbff134e87ba3dbabbc022014
codex-official <77504210+codex-official@users.noreply.github.com>
2024-04-08 19:34:05 +0530

    Update shoppingcart.js
    Fixed Shopping Cart

--- js/shoppingcart.js
+++ js/shoppingcart.js
@@ -1,12 +1,11 @@
 
 
 function displayCart () {
-
-  for (let i=1;i<=localStorage.getItem("itemCount");i++) {
+  let productList = JSON.parse(localStorage.getItem("productList"))
+  for (let i=0;i<productList.length;i++) {
     const parent= document.getElementById('parent2');
     const tr = document.createElement("tr");
     
-
     const td = document.createElement("td");
     td.className = "product__cart__item"
     tr.appendChild(td);
@@ -14,23 +13,21 @@ function displayCart () {
     const div1 = document.createElement('div');
     div1.className = "product__cart__item__pic"
     td.appendChild(div1);
+    
 
     const img = document.createElement('img');
-    img.src = "img/shop/cart/cart-1.jpg"
-    img.src = localStorage
-    .getItem("cartID" + i)
-    .split(",")[2]
-    .match(/(?:"[^"]*"|^[^"]*$)/)[0]
-    .replace(/"/g, "");
+    // console.log(productList[])
+    img.src = JSON.parse(productList[i].img.match(/(?:"[^"]*"|^[^"]*$)/)[0])
     div1.appendChild(img);
+    
 
     const div2 = document.createElement("div");
     div2.className = "product__cart__item__text"
     h6 = document.createElement('h6');
-    h6.innerHTML = localStorage.getItem("cartID" + i).split(",")[0];
+    h6.innerHTML = productList[i].name;
     div2.appendChild(h6);
     h5 = document.createElement('h5');
-    h5.innerHTML = localStorage.getItem("cartID" + i).split(",")[1];
+    h5.innerHTML = productList[i].price
     div2.appendChild(h5);
     td.appendChild(div2);
 
@@ -44,16 +41,64 @@ function displayCart () {
 
     const div4 = document.createElement('div');
     div4.className = "pro-qty"
+    spanDec = document.createElement("span")
+    spanDec.innerHTML = "-"
+    spanDec.className = "dec qtybtn"
+    div4.appendChild(spanDec);
     const input = document.createElement('input');
     input.type = 'text'
     input.value = '1'
-    input.id="qtyinput"
-    div4.appendChild(input); 
+    input.className="qtyinput"
+    div4.appendChild(input);
+    spanInc = document.createElement("span")
+    spanInc.innerHTML = "+"
+    spanInc.className = "inc qtybtn"
+    div4.appendChild(spanInc); 
     div3.appendChild(div4);
+    
+    //Decrement to LocalStorage
+
+    spanDec.addEventListener('click',(x)=>{
+      let qtyInput = (x.srcElement.nextSibling);
+      let count = parseInt(qtyInput.value);
+      if(count != 0) {
+        count -= 1;
+      qtyInput.value = count;
+
+      let name = x.srcElement.parentElement.parentElement.parentElement.parentElement.firstElementChild.lastElementChild.firstElementChild.innerHTML
+      let search = productList.find((x)=>x.name===name);
+
+        if(search.qty!=0){
+          search.qty -=1 
+          console.log(search);
+          qtyInput.value = search.qty; 
+        productList = productList.filter((x)=>x.name!==name);
+        productList.push(search);
+        localStorage.setItem("productList", JSON.stringify(productList))
+        }
+      }})
+
+      // Increment to LocalStorage
+
+      spanInc.addEventListener('click',(x)=>{
+        let qtyInput = (x.srcElement.previousSibling);
+        let count = parseInt(qtyInput.value);
+        count += 1;
+        qtyInput.value = count;
+
+        let name = x.srcElement.parentElement.parentElement.parentElement.parentElement.firstElementChild.lastElementChild.firstElementChild.innerHTML
+        let search = productList.find((x)=>x.name===name);
+        search.qty +=1
+        qtyInput.value = search.qty; 
+        productList = productList.filter((x)=>x.name!==name);
+        productList.push(search);
+        localStorage.setItem("productList", JSON.stringify(productList))
+        update();
+
+        })
+
 
     const td3 = document.createElement('td');
-    
-
     td3.innerHTML = "$ 30.00"
     td3.className = "cart__price"
     tr.appendChild(td3);
@@ -62,28 +107,29 @@ function displayCart () {
     td4.className = "cart__close"
     const span = document.createElement('span');
     span.className = "icon_close"
-    span.addEventListener('click', () => {
+
+    // Remove item from cart
+    span.addEventListener('click', (x) => {
+      let selectedItem = x.srcElement.parentElement.parentElement.firstElementChild.lastElementChild.firstElementChild.innerHTML
+        console.log(x.srcElement.parentElement.parentElement.firstElementChild.lastElementChild.firstElementChild.innerHTML)
+        console.log(productList.filter((x)=>x.name !== selectedItem))
+        productList = productList.filter((x)=>x.name !== selectedItem)
+        localStorage.setItem("productList", JSON.stringify(productList));
         tr.remove();
-        localStorage.removeItem("cartID" + i);
     })
+
     td4.appendChild(span);
     tr.appendChild(td4);
-
     parent.appendChild(tr);
-
-
-
-    input.addEventListener("", () => {
-      td3.innerHTML = "$" + parseFloat(localStorage.getItem("cartID" + i).split(",")[1].slice(1)) * parseInt(document.getElementById("qtyinput").value) + ".00"
-    })
-    // td3.innerHTML = parseFloat(localStorage.getItem("cartID" + i).split(",")[1].slice(1)) * parseInt(document.getElementById("qtyinput").value)
-    console.log(parseFloat(localStorage.getItem("cartID" + i).split(",")[1].slice(1)))
     
-
   }
-        
-    
-}
-    
+}  
 displayCart();
 
+function update(){
+  inputs=[document.getElementsByClassName("qtyinput")]
+  inputs.map((x)=>{
+    console.log(x.value)
+  })
+}
+update();

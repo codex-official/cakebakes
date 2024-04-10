0f163c73fdf920817c04decd2215a7d4a630af3d
codex-official <77504210+codex-official@users.noreply.github.com>
2024-04-10 23:36:17 +0530

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,22 +1,31 @@
 let input = document.getElementById('searcher');
+let shopList = JSON.parse(localStorage.getItem('shopList'))
 
-input.addEventListener("input", ()=> {
-var  li, txtValue, a, i;
 
+input.addEventListener("input", () => {
+    console.log(shopList[0]);
+    var li, txtValue, a, i;
     let filter = input.value.toUpperCase()
-    var li = document.getElementsByClassName('product__item__text');
-        for (i = 0; i < li.length; i++) {
-            a = li[i].getElementsByTagName("h6")[0].firstElementChild;
-            txtValue = a.textContent || a.innerText;
-            console.log(filter);
-            
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            console.log(li[i].parentElement.parentElement)
+    const parent= document.getElementById('parent');
+    var li = parent.getElementsByClassName('product__item__text');
+    // console.log(li);
+    
+    for (i = 0; i < shopList.length; i++) {
+        a = shopList[i];
+        txtValue = a;
+        console.log(li)
+
+        a = li[i].getElementsByTagName("h6")[0].firstElementChild;
+        txtValue = a.textContent || a.innerText;
+        
+
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            // console.log(li[i].parentElement.parentElement)
             li[i].parentElement.parentElement.style.display = "";
-            } else {
-                li[i].parentElement.parentElement.style.display = "none";
-            }
+        } else {
+            li[i].parentElement.parentElement.style.display = "none";
         }
+    }
 })
 
 

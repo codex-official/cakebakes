e36aca018df8367b607155e6a175c1074602fb25
codex-official <77504210+codex-official@users.noreply.github.com>
2024-04-12 00:52:03 +0530

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,32 +1,45 @@
+
+
 let input = document.getElementById('searcher');
 let shopList = JSON.parse(localStorage.getItem('shopList'))
+var li, txtValue, a, i;
+const parent= document.getElementById('parent');
+var li = parent.getElementsByClassName('product__item__text');
 
+input.addEventListener("input", search)
 
-input.addEventListener("input", () => {
-    console.log(shopList[0]);
-    var li, txtValue, a, i;
+function search() {
     let filter = input.value.toUpperCase()
-    const parent= document.getElementById('parent');
-    var li = parent.getElementsByClassName('product__item__text');
-    // console.log(li);
+
     
     for (i = 0; i < shopList.length; i++) {
         a = shopList[i];
         txtValue = a;
-        console.log(li)
 
+        console.log(li)
         a = li[i].getElementsByTagName("h6")[0].firstElementChild;
         txtValue = a.textContent || a.innerText;
         
 
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            // console.log(li[i].parentElement.parentElement)
+            
             li[i].parentElement.parentElement.style.display = "";
         } else {
             li[i].parentElement.parentElement.style.display = "none";
         }
     }
+    localStorage.removeItem("searchQuery");
+}
+
+$("#search-icon").on("click", function () {
+    search();
 })
+
+
+
+
+
+
 
 
 
